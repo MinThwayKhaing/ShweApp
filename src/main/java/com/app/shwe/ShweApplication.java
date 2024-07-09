cc2eefe2e7ca152de9ee69416f956e98d01e2e65
Min Thway Khaing <110376802+MinThwayKhaing@users.noreply.github.com>
2024-07-09 12:56:06 +0630

    Initial commit

---
+++ src/main/java/com/app/shwe/ShweApplication.java
@@ -0,0 +1,13 @@
+package com.app.shwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShweApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/shwe/ShweApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.shwe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShweApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
